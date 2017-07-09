f74ecaf9d203ae274fd3425d57508e2c98c3f3e3
Tomasz Cieśla <ciesla.tomasz.92@gmail.com>
2017-07-09 19:56:23 +0200

    added post, delete, get methods for task resource

--- src/main/java/pl/tciesla/organizer/endpoint/TaskResource.java
+++ src/main/java/pl/tciesla/organizer/endpoint/TaskResource.java
@@ -2,14 +2,16 @@ package pl.tciesla.organizer.endpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.tciesla.organizer.model.Task;
 import pl.tciesla.organizer.repository.TaskRepository;
 
 import java.util.List;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 @RequestMapping("/api")
@@ -21,6 +23,23 @@ public class TaskResource {
     @RequestMapping(value = "/tasks", method = GET)
     public List<Task> tasks() {
         return taskRepository.findAll();
+    }
+
+    @RequestMapping(value = "/tasks/{id}", method = GET)
+    public Task task(@PathVariable long id) {
+        return taskRepository.find(id).orElse(null);
+    }
+
+    @RequestMapping(value = "/tasks", method = POST)
+    public Task saveTask(@RequestParam String name) {
+        Task task = new Task(taskRepository.nextId(), name);
+        taskRepository.save(task);
+        return task;
+    }
+
+    @RequestMapping(value = "/tasks/{id}", method = DELETE)
+    public void deleteTask(@PathVariable long id) {
+        taskRepository.delete(id);
     }
 
 }
